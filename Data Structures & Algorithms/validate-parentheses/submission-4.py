2b9f2518dd7066b3289780e96f3d4afc9a11b2e7
Seth Johnson <113936594+sjohn198@users.noreply.github.com>
2026-07-09 09:13:00 -0700

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        n = len(s)
+        if n % 2 == 1:
+            return False
+
+        open2close = {
+            "(": ")",
+            "[": "]",
+            "{": "}"
+        }
+        opens = ["(", "[", "{"]
+        closes = ["}", ")", "]"]
+    
+        mid = int(n / 2)
+
+        open_stack = []
+        for ch in s:
+            if ch in opens:
+                open_stack.append(ch)
+            else:
+                try:
+                    if ch != open2close[open_stack.pop()]:
+                        return False
+                except Exception:
+                    return False
+
+        return len(open_stack) == 0
